f611396e6af2e0ef18f28938123c91dddbadfaa3
JEAN-PIERRE MENA <pythonrubylang@gmail.com>
2016-05-21 12:15:55 +0200

    Adaptation à OpenSSH 7 avec l'option pour les clés anciennes

--- vnc/priseenmain.sh
+++ vnc/priseenmain.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-export Host=xxx.xxxx.xxxx.xxxx
-export Port=xxxx
-export User="zzzzzzzz"
+export Port="Port Ouvert Pour la Box"
+export User="UtilisateurConnecte"
 export IdentityFile="$HOME/.ssh/id_dsa"
 
 demarrer_xvnc_server(){
-	ssh -i $IdentityFile $User@$Host -p $Port <<EOF
+	ssh -i $IdentityFile $User@$Host -p $Port -o PubkeyAcceptedKeyTypes=+ssh-dss<<EOF
 	x11vnc -many -rfbauth ~/.vnc_passwd
 EOF
 }
 
 se_connecter_en_mode_console(){
-	echo "commande passée: ssh -i $IdentityFile $User@$Host -p $Port"
-	gnome-terminal -x bash -c "ssh -i $IdentityFile $User@$Host -p $Port"
+	echo "commande passée: ssh -i -o PubkeyAcceptedKeyTypes=+ssh-dss $IdentityFile $User@$Host -p $Port"
+	gnome-terminal -x bash -c "ssh -i $IdentityFile -o PubkeyAcceptedKeyTypes=+ssh-dss $User@$Host -p $Port"
 }
 
 demarrer_le_client(){
-	gnome-terminal -x bash -c "ssh -p 9222 -X -L 5900:localhost:5900 ${Host}"
+	gnome-terminal -x bash -c "ssh -p 9222 -X -L 5900:localhost:5900 -o PubkeyAcceptedKeyTypes=+ssh-dss ${Host}"
 	sleep 5
 	xvnc4viewer localhost:5900
 }
