a4a554797fec9228c782272d4f2bbf53c6f040e4
javaskater <pythonrubylang@gmail.com>
2017-08-01 19:26:41 +0200

    Conversion working, still need to make variable work in sed expressions

--- CMS/Wordpress/AND/convertMysqlFromLocalToAND.sh
+++ CMS/Wordpress/AND/convertMysqlFromLocalToAND.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+DIR_DUMPS="${HOME}/CONSULTANT/wordpress"
+LOCAL_MYSQL_DUMP="blogjpmena.sql"
+REMOTE_MYSQL_DUMP="blogand.sql"
+TMP_MYSQL_DUMP="tmp_${REMOTE_MYSQL_DUMP}"
+COLLATE_LOCAL="utf8mb4_unicode_520_ci"
+COLLATE_AND="utf8mb4_general_ci"
+URL_LOCAL="blogjpmena.and"
+URL_AND="blog.jpmena.eu"
+
 trace(){
 	msg=$1
 	timestamp=$(date +%Y/%m/%d-%H:%M:%S)
@@ -12,30 +21,34 @@ DRUPAL_SITE_HOME=$HOME/sites_jpm/d8rif
 #The directory where the import's csv files are located ....
 IMPORT_DIR=$(dirname $DRUPAL_SITE_HOME)/importations
 
-LOG_DIR=$(dirname $DRUPAL_SITE_HOME)
+LOG_DIR=$(pwd)
 LOGFILE=$LOGDIR/$(basename $0)_$(date +%Y%m%d_%H%M%S).log
 
 # changing encodings acccording to
 ## https://stackoverflow.com/questions/30720078/utf8mb4-issue-uploading-to-1and1-com
+## see README.md
+##
 function convert_encodings(){
-   trace "TODO $0 / ${FUNCNAME[0]}"
+   trace "DOING $0 / ${FUNCNAME[0]}"
+	sed -re 's/utf8mb4_unicode_520_ci/utf8mb4_general_ci/g' $1 > $2
 }
 
 function change_url_home(){
-   trace "TODO $0 / ${FUNCNAME[0]}"
+   trace "DOING $0 / ${FUNCNAME[0]}"
+	sed -re 's/blogjpmena.and/blog.jpmena.eu/g' $1 > $2
 }
 
 
 main(){
-	trace "I import Randonnees de Jour from $IMPORT_DIR/randonnees.csv"
-
-
-	if [ $? -eq 0 ]; then
-		trace " - Randonnees de Jour's Erasing : OK"
-	else
-		trace " - Randonnees de Jour's Erasing : KO (see ${LOGFILE})"
-		exit 1
-	fi
+	old_pwd=$(pwd)
+	cd $DIR_DUMPS
+	
+	convert_encodings "${LOCAL_MYSQL_DUMP}" "${TMP_MYSQL_DUMP}"
+	change_url_home "${TMP_MYSQL_DUMP}" "${REMOTE_MYSQL_DUMP}"
+	rm -f "${REMOTE_MYSQL_DUMP}.gz"
+	gzip "${REMOTE_MYSQL_DUMP}"
+	rm -f "${TMP_MYSQL_DUMP}"
+	cd $old_pwd
 }
 
 main | tee $LOGFILE
